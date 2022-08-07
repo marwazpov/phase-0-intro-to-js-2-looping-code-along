f86bf6d64f035024df68bf9d17063e3c15322366
marwa <mds08@mail.aub.edu>
2022-08-07 17:43:15 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+let names=["Guadalupe", "Ollie", "Aki"];
+let giftType= "Surprise";
+
+
+function writeCards(names, giftType) {
+    const thankYouMessages = []
+    for (let n = 0; n < names.length; n++) {
+        let message = `Thank you, ${names[n]}, for the wonderful ${giftType} gift!`
+        thankYouMessages.push(message)
+    };
+    return thankYouMessages
+};
+
+let positiveNumber= 10;
+function countDown(positiveNumber){
+    while (positiveNumber>=0){
+        console.log (positiveNumber--);
+    }
+}
